cd8c9c950fac01add4cf132440aff2cfed1d85d5
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-06-09 20:43:45 -0400

    commit tree e9b485c553f439617dd0a0af56067114148b2e95

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+const newArray = [];
+
+
+function writeCards(cardNames, event) {
+    for (let i = 0; i < cardNames.length; i++) {
+        newArray.push(`Thank you, ${cardNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+
+
+
+function countDown(givenNumber) {
+    let i = 0
+    while (i < givenNumber) {
+        console.log(givenNumber - i);
+        i++;
+    }
+    console.log(givenNumber - givenNumber);
+}
